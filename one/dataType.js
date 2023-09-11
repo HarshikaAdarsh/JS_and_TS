b0da9491cae3f21c8ce20aab6beaf73e59b4c980
HarshikaAdarsh <92363685+HarshikaAdarsh@users.noreply.github.com>
2023-09-11 20:44:35 +0530

    datatypeconversion

---
+++ one/dataType.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version 
+/*
+alert(3+3) = alert can be used in browser but in node we use with diffrent syntax
+
+datatypes
+number = 2^53 range
+bigint = when numbeer is very big 
+string = " "
+boolean= true/false
+null = datatype as well as standlone value (representation of empty value ) 
+undefine = when value is not assign 
+symbol = for unique
+
+object 
+*/
+
+let name = " harshika";
+let age = 19;
+let isloggedIn = false;
+
+console.log(typeof undefined); //its output will be undefine
+console.log(typeof null); // its output is object
---
+++ one/dataTypeConversion.js
@@ -0,0 +1,33 @@
+let score = "33ghfy";
+
+console.log(typeof score); 
+console.log(typeof(score)); // input score as a method 
+
+let valueInNumber = Number(score) // converting score into number
+console.log(typeof valueInNumber);  //datatype conversion
+console.log(valueInNumber); // it will who 'NAN' which is not a number
+
+/*
+ what is actually convertable 
+
+ "33" - from string to no = 33
+ "33abc" - cant be convertable = NAN
+ true - from boolean to number = 1
+*/
+
+
+//boolean
+let isloggedIn = 1;
+ 
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+// 1 = 'true'
+// "" = false
+// "hitesh" = true
+
+//string
+let number = 33;
+
+let valueofnumber = String(number)
+console.log(valueofnumber);
+console.log(typeof (valueofnumber))
