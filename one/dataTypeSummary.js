928babc33edb101030daf6766b406f8f5cdee957
HarshikaAdarsh <92363685+HarshikaAdarsh@users.noreply.github.com>
2023-09-17 12:48:01 +0530

    commit

---
+++ one/dataTypeSummary.js
@@ -0,0 +1,22 @@
+/* 
+Primitive data type => 7
+  String 
+  Number
+  Boolean
+  Null
+  Undefined
+  Symbol => to make anything unique
+  BigInt
+
+Non-Primitive ( Reference type ) =>3
+  Array
+  Objects
+  Functions
+
+*/
+
+//JavaScript is dynamically type language 
+//reason - which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+//example = const score = 100 ; , const score = false ; ,const score = "winner" ; 
+
+
