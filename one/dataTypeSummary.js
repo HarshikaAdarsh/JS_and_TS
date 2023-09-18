60da30646641bc23edd49fdc8d4eaf08e6a64a0b
HarshikaAdarsh <92363685+HarshikaAdarsh@users.noreply.github.com>
2023-09-18 20:31:35 +0530

    memory Management

--- one/dataTypeSummary.js
+++ one/dataTypeSummary.js
@@ -19,4 +19,38 @@ Non-Primitive ( Reference type ) =>3
 //reason - which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
 //example = const score = 100 ; , const score = false ; ,const score = "winner" ; 
 
+/*
+String  =>  const score = "winner";
 
+Number  =>  const score = 100.67;
+
+Boolean =>  const score = false;
+
+Null    => const score = null;
+
+Undefined  => let score; //which is undefined if u do not assign any value 
+
+Symbol => const id = Symbol('123')
+          const anotherId = Symbol('123');
+          console.log(id===anotherId);   //they will never be equal even if we have assign same value in symbol, it is always unique and never same 
+
+BigInt  => const bigNumber = 63285646289794229874365n  //  using 'n' at last represents bigInt
+          */          
+
+
+
+// Non-Primitive
+
+/*  
+Array = const book=["course","fiction","non-fiction"]
+object =  let myobject = {
+    name : "alex",
+    age : 19
+} 
+const myFunction  = function(){
+       console.log("hello world")
+}
+*/
+
+// const bigNumber = 63285646289794229874365n
+// console.log(typeof bignumber)
---
+++ one/memory.js
@@ -0,0 +1,21 @@
+///  STACK     (Primitve)
+//   HEAP      (Non-Primitive )
+
+let ytname = "dumpcoder";
+let anothername = ytname;
+anothername = "dmpyycoder";
+console.log(ytname);
+console.log(anothername);
+console.log(anothername);
+
+let userone ={
+
+    name: "dumppy",
+    upi: "893487@ybl",
+    email: "userone@gmail.com"
+
+}
+
+console.log(userone);
+let usertwo = userone;
+console.log(usertwo);
