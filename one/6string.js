7958ce8eaa954a662751d6ade635f47702c6d2fb
HarshikaAdarsh <92363685+HarshikaAdarsh@users.noreply.github.com>
2023-09-19 17:59:53 +0530

    commit

--- one/6string.js
+++ one/6string.js
@@ -4,3 +4,10 @@
  const repoCount=39;
 
  console.log(name+ repoCount);
+
+ //string Interpolation = here we create placeholder and inject values
+ // using back ticks
+ 
+ console.log(`Hello my name is ${name} amd my Repo Count is ${repoCount}`);
+
+ const gameName = new String ('alexxxx') // key value pairs 
