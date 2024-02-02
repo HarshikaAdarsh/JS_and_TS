9239efd2c32a71136f9360a9f5602d28d04a6a81
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-02 19:07:07 +0530

    updation

--- one/02dataType.js
+++ one/02dataType.js
@@ -2,21 +2,23 @@
 /*
 alert(3+3) = alert can be used in browser but in node we use with diffrent syntax
 
-datatypes
+primitive datatypes
 number = 2^53 range
 bigint = when numbeer is very big 
 string = " "
 boolean= true/false
 null = datatype as well as standlone value (representation of empty value ) 
 undefine = when value is not assign 
-symbol = for unique
+symbol = for uniqueness 
+
+non-primitive
 
 object 
 */
 
-let name = " harshika";
+let name = " alex ";
 let age = 19;
 let isloggedIn = false;
 
 console.log(typeof undefined); //its output will be undefine
-console.log(typeof null); // its output is object
+console.log(typeof null); // its output is object that means null is a type of object 
