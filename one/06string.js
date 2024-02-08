20bd8f66ccb14f7384c1ac7fec899654b49b0773
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-08 23:28:54 +0530

    Update 06string.js

--- one/06string.js
+++ one/06string.js
@@ -12,8 +12,28 @@
 
  const gameName = new String ('alexxxx') // key value pairs // declaration like object
 // in output it will show like an array but it is not it is a object in key value pair 
+// this is helpful in various method usage like indexof() etc
+
  console.log(gameName[0]);
  console.log(gameName.__proto__);
 
  console.log(gameName.length);
  console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('e'));
+
+ //dividing into substring
+ const newString =  gameName.substring(0,4)
+ console.log(newString);
+
+ const anotherString = gameName.slice(-8,3)
+ console.log(anotherString);
+
+ const newStringOne = "    Alex   "
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+
+ const url = "https://alex%20johnson"
+
+ console.log(url.replace('%20', '-'))
+ console.log(url.includes('alex'))
