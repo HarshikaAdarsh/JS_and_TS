1b9fe3839a7d152be6d5924279fe4f877ba73a2b
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-10 22:27:01 +0530

    Update 01arrays.js

--- two/01arrays.js
+++ two/01arrays.js
@@ -1,1 +1,13 @@
-const myArr = [0, 1, 2, 3, 4, 5, 6]
+const myArr = [9, 2, 3, 4, 5, 6]
+console.log(myArr[0]) 
+
+// myArr.push(7)
+// myArr.push(1)
+// myArr.pop()
+
+myArr.unshift(0) // use to shift array values by 1
+// unshift will add element in first position 
+myArr.unshift(9)
+myArr.shift()
+
+console.log(myArr);
