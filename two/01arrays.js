b8f457ec637b75abc800b1eb18d07627ad0eaf93
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-13 21:05:03 +0530

    array

--- two/01arrays.js
+++ two/01arrays.js
@@ -8,6 +8,24 @@ console.log(myArr[0])
 myArr.unshift(0) // use to shift array values by 1
 // unshift will add element in first position 
 myArr.unshift(9)
-myArr.shift()
+myArr.shift() // it will shift array towards left
 console.log(myArr);
-myArr.indexOf(3)
+ console.log(myArr.indexOf(9));
+
+ const newArr = myArr.join()
+
+ console.log(myArr);
+ console.log(newArr);
+
+ // Slice ,  Splice
+
+ console.log("A", myArr);
+
+ const myn1 = myArr.slice(1, 5) // slice shows the last element of range 
+
+ console.log(myn1);
+ console.log("B", myArr);
+
+ const myn2 = myArr.splice(1,5) // splice shows the last element of range 
+console.log("C", myArr);
+ console.log(myn2);
