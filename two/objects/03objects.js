9bd092d54732bc12f2880edfc1bfcb2ea80fda08
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-14 14:18:10 +0530

    Create 03objects.js

---
+++ two/objects/03objects.js
@@ -0,0 +1,18 @@
+//destructuring
+const course = {
+    coursename: "Js ",
+    price: "499",
+    courseInstructor: "Stephen"
+}
+// course.courseInstructor
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+
+
+// react concept 
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "alex")
