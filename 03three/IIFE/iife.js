9a44801ad503033ca1457aae0453e99cf47d1275
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-16 13:01:22 +0530

    Update iife.js

--- 03three/IIFE/iife.js
+++ 03three/IIFE/iife.js
@@ -1,7 +1,14 @@
 //Immediatley invoked fucntion expressions (IIFE)
+// syntax of IIFE => ()()
+// where (fucntion defination )(fucntion execution call )
+
+// why IIFE = to reduce the pollution created by global scope 
 
 
 (function chai() {
     console.log(`DB CONNECTED`);
 })
 ()
+
+
+// sytax of IIFE using Arrow fucn
