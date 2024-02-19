6becaa589a50d3c5908d526b2785ef14cbca6102
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-19 23:08:48 +0530

    Update 03forOf.js

--- 05Iterator/03forOf.js
+++ 05Iterator/03forOf.js
@@ -19,3 +19,7 @@ map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('fr', "France")
 console.log(map);
+// forOf in map 
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
