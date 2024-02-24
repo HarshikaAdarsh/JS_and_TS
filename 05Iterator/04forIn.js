490a067cdd7908c6568bb3cb21c4bb7fb4dddcb1
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-24 23:10:14 +0530

    Update 04forIn.js

--- 05Iterator/04forIn.js
+++ 05Iterator/04forIn.js
@@ -1,5 +1,9 @@
 const myObject = {
     js: 'javascript',
     cpp:"ruby",
-    swift
+    swift : 'swift'
+}
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+    console.log(myObject[key]);
 }
