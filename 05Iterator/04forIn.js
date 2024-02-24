554eefa6629352dd9b2d7aca838a1e863f4f6cd3
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-24 23:21:20 +0530

    Update 04forIn.js

--- 05Iterator/04forIn.js
+++ 05Iterator/04forIn.js
@@ -7,3 +7,7 @@ for (const key in myObject) {
     console.log(`${key} shortcut is for ${myObject[key]}`);
     console.log(myObject[key]);
 }
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    console.log( key  );
+}    
