a66cf025348cae50c40d462a5e22c5f3a8d9957f
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-25 21:13:54 +0530

    for each

--- 05Iterator/04forIn.js
+++ 05Iterator/04forIn.js
@@ -11,3 +11,14 @@ const programming = ["js", "rb", "py", "java", "cpp"]
 for (const key in programming) {
     console.log( key  );
 }    
+
+const map = new Map()
+map.set('IN', "India")
+map.set('IN', "United States of America")
+map.set('IN', "France")
+map.set('IN', "India")
+
+for (const key in map) { // we cannot apply Iterative in map
+    console.log(key);
+}
+
