fdf178348097a6981618613a34d866aa14cd4ea4
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-03-02 20:06:28 +0530

    Update 05forEach.js

--- 05Iterator/05forEach.js
+++ 05Iterator/05forEach.js
@@ -2,4 +2,34 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 
 // callback function do not have function name
 
-coding.forEach( function (val) {} )
+//coding.forEach( function (val) {} )
+
+// for each do not return any value 
+// prove =>
+const values = coding.forEach((item) => {
+    console.log(item);
+    return item;
+}) 
+
+// to overcome this we have another way which is filter
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const newNum = myNums.filter((num) => {
+    num > 4
+})
+console.log(newNum); // this will give empty array [REASON]=> explicit return (discussed in arrow function)
+// which means if we are using  {}scope the we have to return it 
+
+const newNum2 = myNums.filter((num) => num > 4)
+console.log(newNum2);
+
+//-----------------------------------------------------------------------------------------------------------------------------//
+
+//now try to implement it in forEach here we have to use if else
+
+myNums.forEach((num) => {
+    if(num>6){
+        newNum.push(num)
+    }
+})
+console.log(newNum);
