29abc93b889157d57fad723df97024cc1ab6569e
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-03-28 21:45:44 +0530

    project4

---
+++ 07Projects/03project(DigitalClock)/script.js
@@ -0,0 +1,9 @@
+
+// document.querySelector('#clock')
+
+const clock  =  document.getElementById('clock');
+
+setInterval(function () {
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000)
