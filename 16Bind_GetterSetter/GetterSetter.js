016b21ddf245ac7bcbc7d66e84517c38e58ca4f3
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-07-09 21:22:26 +0530

    getter and setter

---
+++ 16Bind_GetterSetter/GetterSetter.js
@@ -0,0 +1,16 @@
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+   // getter and setter works together
+    get password(){
+        return this.password.toUpperCase()
+    }
+    set password(value){
+        this.password = value.toUpperCase() // because of this "this" here it will show
+        // error  'Maximum call stack size exceeded' as set's this and constructor this will execute back to back
+    }
+}
+const hitesh = new User("hitesh.ai")
+console.log(hitesh.password);
